00e05c50965e2c32328ffab8353d88158847c451
Okiosk <victorouzaud@gmail.com>
2025-10-29 15:27:03 +0100

    # message hide when quest complete

--- UntitledFlowerGame/Assets/InteractableScript.cs
+++ UntitledFlowerGame/Assets/InteractableScript.cs
@@ -6,7 +6,12 @@ public class InteractableScript : MonoBehaviour
 {
     
     [SerializeField] CollectibleType collectibleTarget;
-    
+    [SerializeField] GameObject loveEffect;
+    [SerializeField] GameObject badEffect;
+
+    [SerializeField] SelfDialogue selfDialogueScript;
+    [SerializeField] LinkedDialogue linkedDialogueScript;
+    [SerializeField] LinkedDialogueInfo linkedDialogueInfoScript;
     // Start is called before the first frame update
     public CollectibleType getCollectibleTarget()
     {
@@ -15,9 +20,21 @@ public class InteractableScript : MonoBehaviour
 
     public void OnQuestComplete()
     {
-        Debug.Log("Quest Success");
+        GameObject effect = Instantiate(loveEffect, transform.position + Vector3.up, Quaternion.identity, transform);
+        if (selfDialogueScript != null)
+        {
+            selfDialogueScript.HideComplitelyBubble();
+        }
+        if (linkedDialogueScript != null)
+        {
+            linkedDialogueScript.HideComplitelyBubble();
+        }
+        if (linkedDialogueInfoScript != null)
+        {
+            linkedDialogueInfoScript.HideComplitelyBubble();
+        }
     }
-    
+
     public void OnQuestFailed()
     {
         Debug.Log("Quest failed");
