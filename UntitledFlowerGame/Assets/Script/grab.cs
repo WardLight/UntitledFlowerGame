f032ec135427825dbeb7060c5d6dadd23d4dda62
Okiosk <victorouzaud@gmail.com>
2025-10-29 03:55:54 +0100

    + ajout dialogue à 1 pnj

--- UntitledFlowerGame/Assets/Script/grab.cs
+++ UntitledFlowerGame/Assets/Script/grab.cs
@@ -4,53 +4,32 @@ using UnityEngine;
 
 public class grab : MonoBehaviour
 {
-    public bool hold;
-    public KeyCode grabKey; 
-    public bool canGrab;
+    public KeyCode grabKey;
 
-
-    /*
+    private bool hold;
     void Update()
     {
         
-        if (canGrab)
+
+        if (Input.GetKey(grabKey))
         {
-            if (Input.GetKey(grabKey))
-            {
-                hold = true;
-                transform.position = new Vector3(transform.position.x + (1f * Time.deltaTime), transform.position.y, transform.position.z);
-            }
-            else
-            {
-                hold = false;
-                Destroy(GetComponent<FixedJoint>());
-            }
+            hold = true;
         }
-    }
-    */
-    void Update()
-    {
-        if (!hold)
+        else
         {
+            hold = false;
             Destroy(GetComponent<FixedJoint>());
         }
+        
     }
 
     private void OnCollisionEnter(Collision col)
     {
-        print("hey");
-        if (hold && col.transform.tag != "Player")
+        if (hold)
         {
             Rigidbody rb = col.transform.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                FixedJoint fj = transform.gameObject.AddComponent(typeof(FixedJoint)) as FixedJoint;
-                fj.connectedBody = rb;
-            }
-            else
-            {
-                FixedJoint fj = transform.gameObject.AddComponent(typeof(FixedJoint)) as FixedJoint;
-            }
+            FixedJoint fj = transform.gameObject.AddComponent(typeof(FixedJoint)) as FixedJoint;
+            fj.connectedBody = rb;
         }
     }
 }
